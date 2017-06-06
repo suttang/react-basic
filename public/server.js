a67ff63bcd4dd081646cec8bf84d847278ed4031
suttang <suttang@gmail.com>
2017-06-06 17:52:53 +0900

    fix standard format

--- public/server.js
+++ public/server.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack');
-const webpackDevServer = require('webpack-dev-server');
-const config = require('webpack.config.dev');
-const argv = require('minimist')(process.argv.slice(2));
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('webpack.config.dev')
+const argv = require('minimist')(process.argv.slice(2))
 
-const host = argv.host || '0.0.0.0';
-const port = argv.port || 8000;
+const host = argv.host || '0.0.0.0'
+const port = argv.port || 8000
 
 Object.keys(config.entry).forEach(key => {
   config.entry[key].unshift(
@@ -13,28 +13,28 @@ Object.keys(config.entry).forEach(key => {
     `webpack-dev-server/client?http://${host}:${port}`
     // HotModuleReplacementPluginにモジュールの更新を行わせる
     // 'webpack/hot/dev-server'
-  );
-});
+  )
+})
 
 config.plugins = [
   // new webpack.HotModuleReplacementPlugin(),
-  new webpack.NamedModulesPlugin(),
-];
+  new webpack.NamedModulesPlugin()
+]
 
-const compiler = webpack(config);
-const server = new webpackDevServer(compiler, {
+const compiler = webpack(config)
+const server = new WebpackDevServer(compiler, {
   contentBase: './public',
   publicPath: '/',
   stats: { colors: true },
   historyApiFallback: true,
   host: host,
-  hot: true,
-});
+  hot: true
+})
 
 server.listen(port, error => {
   if (error) {
-    console.log(error);
-    return;
+    console.log(error)
+    return
   }
-  console.log(`Listening at ${host}:${port}`);
-});
+  console.log(`Listening at ${host}:${port}`)
+})
